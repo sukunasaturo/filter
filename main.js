f14021c435d0a4127d647d0fed751f2ebc946cf2
sukunasaturo <153446547+sukunasaturo@users.noreply.github.com>
2024-01-17 19:40:23 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,15 @@
+function preload(){
+
+}
+
+function setup(){
+    canvas = createCanvas(400,400);
+    canvas.centre();
+}
+
+function draw(){
+
+    function take_snapshot(){
+        save('myFilterImage.png');
+    }
+}
